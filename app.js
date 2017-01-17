eab10d6eb7524ac4f76b60054c7aaed076552315
Robert Schroeder <robschroeder333@users.noreply.github.com>
2017-01-17 11:18:18 -0500

    server up

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log("Server listening.");
+    console.log('Server listening.');
 });
 
-app.get("/", (req, res) => {
+app.use('/', (req, res, next) => {
+  console.log(`client is using: ${req.method} ${req.url}`);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.write('Welcome to "Better." Our new version of twitter.');
+  res.send();
+  console.log(`status code: ${res.statusCode}`);
+});
+
+app.get('/news', (req, res) => {
+  res.send('Extra! Extra! Read all about it!');
+  console.log(`status code: ${res.statusCode}`);
+});
+
+app.use('/', (req, res) => {
+  res.statusCode = 404;
+  console.log(`status code: ${res.statusCode}`);
+  res.send('sorry, you have not entered a correct url');
 });
